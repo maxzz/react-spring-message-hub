ed675e5da9574b613dd140d506c34c10965959b6
Max Zakharzhevskiy <maxzz@msn.com>
2023-09-30 15:50:59 -0700

    WIP. add alternative solution placeholder w/ tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { AppStateWReactOnly } from './components/1-state-w-react-only';
+import { AppStateWValtio } from './components/2-state-w-valtio';
 
 export function App() {
     return (
-        <div style={{height: '100%', backgroundColor: 'salmon', display: 'grid', gridTemplateRows: '1fr 1fr'}}>
+        <div style={{height: '100%', display: 'grid', gridTemplateRows: '1fr 1fr'}}>
             <AppStateWReactOnly />
+            <AppStateWValtio />
         </div>
     );
 }
---
+++ src/components/2-state-w-valtio/index.tsx
@@ -0,0 +1,7 @@
+export function AppStateWValtio() {
+    return (
+        <div className="bg-red-300 text-red-900 grid place-items-center">
+            TODO: reimplement this as simply as possible with Valtio and Tailwind. Later.
+        </div>
+    );
+}
